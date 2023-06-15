5e429b773053e33228fa804ef2cf6b18535d8013
1001Pranav <pranav.n-at-411977029542>
2023-06-15 20:45:57 +0530

    Initial go push

---
+++ basics/basic.go
@@ -0,0 +1,28 @@
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	
+	fmt.Println("Hello, World!");
+	
+	// Variables and data.
+	// Shorthand operator for assigning variables.
+	message := "Assignment Operator";
+
+	// Integer declarations and multiple declarations
+	var number int;
+
+	var num1, num2 int = 1, 2;
+
+	fmt.Println(message, number, num1, num2);
+
+	fmt.Println("Using the function from other module.. ",rand.Intn(120));
+
+	const CONSTANT_STRING string = "Hello ಜಗತ್ತು";
+	fmt.Println(CONSTANT_STRING);
+}
