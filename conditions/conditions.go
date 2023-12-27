5bfcbb7c1fc1c8e7059f0af2403e3c3568bf5b9c
npranavr <npranavr@gmail.com>
2023-12-27 21:33:53 +0530

    Adding invalid if condition

--- conditions/conditions.go
+++ conditions/conditions.go
@@ -1,26 +1,13 @@
-package condition
+package main
 
 import (
 	"fmt"
 	"strings"
 )
 
-func condition() {
-	var a, c int = 5, 1;
-
-	if (a > c) {
-		// F for format like %d
-		fmt.Printf("%d is greater than %d", a,c);
-	} else if ( c > a) {
-		fmt.Printf("%d is greater than %d", c, a);
-	} else {
-		fmt.Printf("%d is equal", a);
-	}
-
-	fmt.Printf("\n");
+func switchCondition(weekDay string) {
 	var weeks = []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
-	var weekDay = "sunday";
 	switch(strings.ToUpper(weekDay)) {
 		case strings.ToUpper(weeks[0]):
 			fmt.Println(weeks[0]);
@@ -46,4 +33,46 @@ func condition() {
 		default:
 			fmt.Println("Week Not found:");
 	}
+}
+
+func invalidIfCondition() {
+	var x int = 10;
+	var y int = 20;
+	/*
+		if x > y {
+			fmt.Printf("%d is greater than %d\n", x, y);
+		}
+		else if x < y {
+			fmt.Printf("%d is greater than %d\n", y, x);
+		}
+		else {
+			fmt.Printf("%d is equal %d\n", x, y);
+		}
+	*/
+
+	if x > y {
+		fmt.Printf("%d is greater than %d\n", x, y);
+	} else if x < y {
+		fmt.Printf("%d is greater than %d\n", y, x);
+	} else {
+		fmt.Printf("%d is equal %d\n", x, y);
+	}
+}
+
+func main() {
+	var a, c int = 5, 1;
+
+	if (a > c) {
+		// F for format like %d
+		fmt.Printf("%d is greater than %d", a,c);
+	} else if ( c > a) {
+		fmt.Printf("%d is greater than %d", c, a);
+	} else {
+		fmt.Printf("%d is equal", a);
+	}
+
+	fmt.Printf("\n");
+	var weekDay = "sunday";
+	switchCondition(weekDay);
+	invalidIfCondition();
 }
