c59c3d584be74bff500bdcf0d86cedb89552a693
npranavr <npranavr@gmail.com>
2023-12-27 08:38:39 +0530

    Added maps methods

--- maps/maps.go
+++ maps/maps.go
@@ -1,9 +1,31 @@
-package maps
+package main
 import "fmt"
 
-func maps(){
-	var stateCapital = make(map[string]string);
+func main(){
+	var countryCapital map[string]string = map[string]string {
+		"India": "New Delhi",
+		"Pakistan": "Islamabad",
+		"China": "Beijing",
+	}
+
+	fmt.Println(countryCapital["India"]);
+
+	// Deleing key in map
+	delete(countryCapital, "Pakistan");
+
+	countryCapital["Kailasa"] = "";
+	// Accessing value with boolean
+	fmt.Println(countryCapital);
+
+	valKailasa, isKailasaExists := countryCapital["Kailasa"];
+	fmt.Println("Is Kailasa Exists",isKailasaExists, "Value of Kailasa",  valKailasa);
+
+	valEngland, isEnglandExists := countryCapital["England"];
+	fmt.Println("England Exists", isEnglandExists, "Value of England is", valEngland);
+
 	// Map can also declared as
+	var stateCapital = make(map[string]string);
+
 	// var mapExample1 map[key_dataType]value_dataType;
 	stateCapital["Karnataka"] = "Bengaluru";
 	stateCapital["Maharastra"] = "Mumbai";
